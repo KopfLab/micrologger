3e47b1d1fe6e6acc4db6a449d7d61d515b9af49a
Sebastian Kopf <seb.kopf@gmail.com>
2026-04-13 10:10:06 -0600

    update experiments pk in testing scripts

--- setup/db/testing.sql
+++ setup/db/testing.sql
@@ -11,6 +11,14 @@ INSERT INTO experiments(exp_id, group_id, user_id, recording)
 OVERRIDING SYSTEM VALUE
 VALUES (1, 'TEST', 'testuser', True), (2, 'TEST', 'testuser', True), (3, 'TEST', 'testuser', False);
 
+-- update primary key sequence
+SELECT setval(
+  pg_get_serial_sequence('experiments', 'exp_id'),
+  (SELECT MAX(exp_id) FROM experiments),
+  true
+);
+
+
 -- Table: experiment_devices
 INSERT INTO experiment_devices(exp_id, core_id)
 VALUES(1, 'test1'), (2, 'test1'), (3, 'test1');
