c0646581c2a6ab7e585e83976626903726627f4d
Sebastian Kopf <seb.kopf@gmail.com>
2026-04-14 21:29:52 -0600

    db read logic

--- R/logic_db_read.R
+++ R/logic_db_read.R
@@ -1,4 +1,25 @@
-#' Retrieve devices
+#' Get user groups
+#' @export
+ml_get_groups <- function(group_id = NULL, con = db()) {
+  # information
+  cli_alert_info(
+    "Retrieving group information{if(!is.null(group_id)) format_inline(' for group{?s} {.field {group_id}}')}..."
+  )
+
+  df <- tbl(con, "groups")
+
+  if (!is.null(group_id)) {
+    group_id <- stringr::str_to_upper(group_id)
+    df <- df |>
+      dplyr::filter(stringr::str_to_upper(group_id) %in% !!group_id)
+  }
+
+  df <- df |> collect()
+  cli_alert_success("Found {nrow(df)} group{?s}.")
+  return(df)
+}
+
+#' Retrieve devices including their structures and who is currently using them
 #' @param group_id devices from which group(s) to fetch
 #' @return devices
 #' @export
@@ -20,10 +41,24 @@ ml_get_devices <- function(
     )
   )
 
-  df <- tbl(con, "devices")
+  df <- tbl(con, "devices") |>
+    left_join(tbl(con, "structures"), by = c("type", "version")) |>
+    left_join(
+      tbl(con, "experiment_devices") |> dplyr::select("core_id", "exp_id"),
+      by = "core_id"
+    ) |>
+    left_join(
+      tbl(con, "experiments") |>
+        dplyr::filter(!archived) |>
+        dplyr::select("exp_id", "user_id", "user_first", "user_last"),
+      by = "exp_id"
+    ) |>
+    slice_max(exp_id, by = "core_id", n = 1L)
 
   if (!is.null(group_id)) {
-    df <- df |> dplyr::filter(group_id %in% !!group_id)
+    group_id <- stringr::str_to_upper(group_id)
+    df <- df |>
+      dplyr::filter(stringr::str_to_upper(group_id) %in% !!group_id)
   }
 
   if (!quo_is_null(filter_quo)) {
@@ -59,7 +94,9 @@ ml_get_experiments <- function(
   df <- tbl(con, "experiments")
 
   if (!is.null(group_id)) {
-    df <- df |> dplyr::filter(group_id %in% !!group_id)
+    group_id <- stringr::str_to_upper(group_id)
+    df <- df |>
+      dplyr::filter(stringr::str_to_upper(group_id) %in% !!group_id)
   }
 
   if (!quo_is_null(filter_quo)) {
@@ -89,42 +126,103 @@ ml_get_experiments <- function(
 
 #' Retrieve experiment devices
 #'
-#' Returns experiment-devices joined with experiment and devices tables so filter conditions can be applied on any of these as well.
+#' Returns experiment-devices merged with devices table
 #'
 #' @inheritParams ml_get_experiments
 #' @return experiments_devices
 #' @export
 ml_get_experiment_devices <- function(
-  group_id = NULL,
+  exp_id = NULL,
   filter = NULL,
-  select = c("exp_id", "core_id", "label", "recording"),
+  select = everything(),
   con = db()
 ) {
+  # safety check
+  exp_id |>
+    check_arg(
+      !missing(exp_id) && is_integerish(exp_id) && length(exp_id) > 0,
+      "must provide at least one experiment id"
+    )
+  select_quo <- enquo(select)
+  filter_quo <- enquo(filter)
+
+  # information
+  cli_alert_info(
+    c(
+      "Retrieving experiment devices for {qty(length(exp_id))}{.field experiment{?s}} {col_magenta(exp_id)}",
+      "{if(!quo_is_null(filter_quo)) format_inline(' with filter {.code {quo_text(filter_quo)}}')}",
+      "..."
+    )
+  )
+
+  df <-
+    tbl(con, "experiment_devices") |>
+    left_join(tbl(con, "devices"), by = "core_id") |>
+    dplyr::filter(exp_id %in% !!exp_id)
+
+  if (!quo_is_null(filter_quo)) {
+    df <- df |> dplyr::filter(!!filter_quo)
+  }
+
+  df <- df |> dplyr::select(!!select_quo) |> collect()
+  cli_alert_success("Found {nrow(df)} experiment device{?s}.")
+  return(df)
+}
+
+#' read data logs
+#' @export
+ml_get_logs <- function(
+  exp_id,
+  filter = NULL,
+  select = everything(),
+  max_rows = NULL,
+  convert_to_TZ = Sys.timezone(),
+  con = db()
+) {
+  # safety check
+  exp_id |>
+    check_arg(
+      !missing(exp_id) && is_integerish(exp_id) && length(exp_id) > 0,
+      "must provide at least one experiment id"
+    )
   filter_quo <- enquo(filter)
   select_quo <- enquo(select)
 
   # information
   cli_alert_info(
     c(
-      "Retrieving experiment devices{if(!is.null(group_id)) format_inline(' for group{?s} {.field {group_id}}')}",
+      "Retrieving logs for {qty(length(exp_id))}{.field experiment{?s}} {col_magenta(exp_id)}",
       "{if(!quo_is_null(filter_quo)) format_inline(' with filter {.code {quo_text(filter_quo)}}')}",
       "..."
     )
   )
 
-  df <- tbl(con, "experiment_devices") |>
-    left_join(tbl(con, "devices"), by = "core_id") |>
-    left_join(tbl(con, "experiments"), by = c("exp_id", "group_id"))
-
-  if (!is.null(group_id)) {
-    df <- df |> dplyr::filter(group_id %in% !!group_id)
-  }
+  df <-
+    tbl(con, "experiment_logs") |>
+    inner_join(tbl(con, "logs"), by = c("log_id")) |>
+    left_join(tbl(con, "experiment_devices"), by = c("exp_id", "core_id")) |>
+    dplyr::filter(exp_id %in% !!exp_id) |>
+    arrange(log_id)
 
   if (!quo_is_null(filter_quo)) {
     df <- df |> dplyr::filter(!!filter_quo)
   }
 
-  df <- df |> arrange(label) |> dplyr::select(!!select_quo) |> collect()
-  cli_alert_success("Found {nrow(df)} experiment device{?s}.")
+  if (!is.null(max_rows)) {
+    df <- df |> dplyr::filter(row_number() <= !!max_rows)
+  }
+
+  df <- df |> dplyr::select(!!select_quo) |> collect()
+  cli_alert_success("Found {nrow(df)} log{?s}.")
+
+  # local TZ conversion
+  if (!is.null(convert_to_TZ) && "log_datetime" %in% names(df)) {
+    cli_alert_info(
+      "Converting {.code log_datetime} to timezone {col_blue(convert_to_TZ)}."
+    )
+    df <- df |>
+      mutate(log_datetime = lubridate::with_tz(log_datetime, convert_to_TZ))
+  }
+
   return(df)
 }
