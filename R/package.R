3e5d69e0ff515c092c9f5f1dc5b2d8ff9c4dac1a
Sebastian Kopf <seb.kopf@gmail.com>
2026-04-14 21:30:16 -0600

    update package metadata

---
+++ R/package.R
@@ -0,0 +1,11 @@
+#' @keywords internal
+"_PACKAGE"
+
+## usethis namespace: start
+#' @import cli
+#' @import rlang
+#' @importFrom tibble tibble
+#' @import dplyr
+#' @import shiny
+## usethis namespace: end
+NULL
